dac4045ac22dbeaf7d810af962f4fb2fc7dda04c
jdaipopo <jdaipopo11@gmail.com>
2022-09-16 13:33:25 +0700

    position card of my to do list and button postion

--- src/Pages/myTodoList.tsx
+++ src/Pages/myTodoList.tsx
@@ -1,11 +1,32 @@
-import { Card } from "primereact/card";
+import Div100vh from "react-div-100vh";
+import "./index.css";
 
 const MyTodoList: React.FC = () => {
   return (
-    <Card>
-      content
-      <button>Add To Do</button>
-    </Card>
+    <Div100vh>
+      <div className="cardlist">
+        <div className="card-flex">
+          MY List I Want To Do
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit sunt voluptatum error voluptas. Reprehenderit
+            delectus, nostrum enim a sed quod doloremque? Praesentium saepe
+            repellendus consequatur rerum itaque sit architecto aut error
+            pariatur deserunt, odit voluptate id placeat excepturi molestiae
+            tempora fugit dignissimos provident, beatae officia nisi. Id
+            deserunt fugit, natus assumenda incidunt porro exercitationem, optio
+            dolor eius quae culpa itaque facilis? Adipisci vel harum eaque
+            reiciendis rem velit corrupti laudantium recusandae. Tenetur
+            molestiae fuga quaerat nostrum provident error ducimus quod
+            perspiciatis? Nisi ab ex nemo quas, magnam omnis dolor obcaecati
+            amet maiores esse est et quidem similique nobis eum enim!
+          </div>
+          <div>
+            <button className="button">Add To Do</button>
+          </div>
+        </div>
+      </div>
+    </Div100vh>
   );
 };
 
