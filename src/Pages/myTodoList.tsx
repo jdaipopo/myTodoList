b7891ab717f2df9265af6d326061c7d9fbfac5b7
jdaipopo <jdaipopo11@gmail.com>
2022-09-16 11:48:47 +0700

    create pages mytodolist

---
+++ src/Pages/myTodoList.tsx
@@ -0,0 +1,12 @@
+import { Card } from "primereact/card";
+
+const MyTodoList: React.FC = () => {
+  return (
+    <Card>
+      content
+      <button>Add To Do</button>
+    </Card>
+  );
+};
+
+export default MyTodoList;
