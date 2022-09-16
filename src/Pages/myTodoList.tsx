9725bb09c8a25bcb91c7b1b14daedf0af5fab49a
jdaipopo <jdaipopo11@gmail.com>
2022-09-16 15:15:07 +0700

    click add list so show dialog on display

--- src/Pages/myTodoList.tsx
+++ src/Pages/myTodoList.tsx
@@ -1,8 +1,28 @@
 import Div100vh from "react-div-100vh";
 import "./index.css";
 import { Button } from "primereact/button";
+import { useState } from "react";
+import { Dialog } from "primereact/dialog";
 
 const MyTodoList: React.FC = () => {
+  const [displayBasic, setDisplayBasic] = useState(false);
+
+  const onClick = () => setDisplayBasic((prev) => !prev);
+
+  const renderFooter = (name: string) => {
+    return (
+      <div>
+        <Button
+          label="No"
+          icon="pi pi-times"
+          onClick={onClick}
+          className="p-button-text"
+        />
+        <Button label="Yes" icon="pi pi-check" onClick={onClick} autoFocus />
+      </div>
+    );
+  };
+
   return (
     <Div100vh>
       <div className="cardlist">
@@ -28,10 +48,29 @@ const MyTodoList: React.FC = () => {
               label="Add List"
               className="p-button-rounded"
               style={{ backgroundColor: "green" }}
+              onClick={onClick}
             />
           </div>
         </div>
       </div>
+
+      <Dialog
+        header="Header"
+        visible={displayBasic}
+        style={{ width: "50vw" }}
+        footer={renderFooter("displayBasic")}
+        onHide={onClick}
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </Dialog>
     </Div100vh>
   );
 };
